d056f98fea967c5102c66563a24d0d84ce7c91d2
Isaac Campos <kakoo26i@gmail.com>
2016-03-15 00:30:56 +0000

    el botón de realizar calculo convierte los valores de las matrices en un array de JavaScript

--- codigoJavaScript.js
+++ codigoJavaScript.js
@@ -15,16 +15,28 @@ jQuery(document).ready(function($){
             var fila_temp = entrada_temp.charAt(0);
             var columna_temp = entrada_temp.charAt(1);
             if (fila_temp <= fila_hover && columna_temp <= columna_hover){
-                $("[entrada='"+entrada_temp+"']").addClass("on");
+                var $_entrada_temp = $("[entrada='"+entrada_temp+"']");
+                if ($("[entrada='"+entrada_temp+"']").hasClass("blocked")){
+                    $("[entrada='"+entrada_temp+"']").addClass("on2");
+                } else {
+                    $("[entrada='"+entrada_temp+"']").addClass("on");
+                }
             } else {
-                $("[entrada='"+entrada_temp+"']").removeClass("on");
+                if ($("[entrada='"+entrada_temp+"']").hasClass("blocked")){
+                    $("[entrada='"+entrada_temp+"']").removeClass("on2");
+                } else {
+                    $("[entrada='"+entrada_temp+"']").removeClass("on");
+                }
             }
-        })
-        $("#cont_selector_tamanio .TBODY td").empty();
-        $("#cont_selector_tamanio [entrada='"+entrada_hover+"']").html(fila_hover+"x"+columna_hover);
+        });
 	});
     
     $("#cont_selector_tamanio .TBODY td").click(function() {
+        $("#cont_selector_tamanio .TBODY td").removeClass("blocked").removeClass("clicked").removeClass("on2");
+        $(this).addClass("clicked").trigger('mouseenter');
+        
+        $("#cont_selector_tamanio .TBODY td").addClass("blocked");
+
         var letra_tabla = $("#selector_tamanio").val();
         var entrada = $(this).attr("entrada");
         var fila_M = entrada.charAt(0); // attr("entrada") return string "{i}{j}";
@@ -33,10 +45,19 @@ jQuery(document).ready(function($){
 	});
     
     $("#selector_tamanio").change(function(){
-        $("#cont_selector_tamanio .TBODY td").removeClass("blocked").removeClass("on").empty();
+        var letra_tabla = $(this).val();
+        var cant_filas = $("#matriz_"+letra_tabla+" tr").length;
+        var cant_columnas = $("#matriz_"+letra_tabla+" tr:first td").length;
+        
+        $("#cont_selector_tamanio [entrada='"+cant_filas+""+cant_columnas+"']").click(); //Simula el evento de click
     });
     
-    $("#selector_operador").change(function(){
+    $("#cont_selector_tamanio table").mouseout(function(){
+        $("#cont_selector_tamanio .TBODY td").removeClass("on2");
+    });
+    
+    
+    $("#selector_operacion").change(function(){
         if($(this).val() == "inv"){
             mostrarSelectInv();
         } else if($(this).val() == "*n"){
@@ -61,15 +82,15 @@ jQuery(document).ready(function($){
     }
     
     function generarMatrizHTMLVacia(letra_matriz, M, N){
-        matriz_HTML =
+        var matriz_HTML =
             '<label>Matriz '+letra_matriz+' <sub>'+M+'x'+N+'</sub>'+'</label><br>\
             <form class="cont_matriz" id="matriz_'+letra_matriz+'">\
                 <div class="DIV_6">\
                     <table class="TABLE">\
                         <tbody class="TBODY">';
-                            for(i=0; i < M; i++){
+                            for(var i=0; i < M; i++){
                                 matriz_HTML += '<tr>';
-                                for(j=0; j < N; j++){
+                                for(var j=0; j < N; j++){
                                     matriz_HTML += '<td><input name="'+i+''+j+'"></td>';
                                 }
                                 matriz_HTML += '</tr>';
@@ -83,17 +104,40 @@ jQuery(document).ready(function($){
     }
     
     get_data = function(){
-        var matriz_A = [];
+        var matriz_A = getMatriz("A");
+        var matriz_B = getMatriz("B");
         
-        $("#cont_matriz_A input").each(function(matriz_A){
+        console.log(matriz_A);
+        alert(matriz_A);
+        console.log(matriz_A);
+        alert(matriz_A);
+        
+    };
+    
+    function crearArrayBidimensionalVacio(cant_filas, cantiColumnas){
+        var matriz = new Array(cant_filas);
+        for (var i = 0; i < cant_filas; i++) {
+          matriz[i] = new Array(cantiColumnas);
+        }
+        return matriz;
+    }
+    
+    function getMatriz(letra){
+        var cant_filas = $("#cont_matriz_"+letra+" tr").length;
+        var cant_columnas = $("#cont_matriz_"+letra+" tr:first td").length;
+        var matriz = crearArrayBidimensionalVacio(cant_filas, cant_columnas);
+        
+        $("#cont_matriz_"+letra+" input").each(function(){
             var entrada = $(this).attr("name");
             var i = entrada.charAt(0);
             var j = entrada.charAt(1);
-            matriz_A[i][j] = $(this).val();            
-        });
-                                       
-        alert(matriz_A);
-        console.log(matriz_A);
+            matriz[i][j] = $(this).val();
+        });   
+        return matriz;
+    }
+    
+    function validarOperacionMatrices(){
+        var operacion;
     }
     
     //SE INICIAN LAS MATRICES CON TAMAÑO 5x5
@@ -101,9 +145,9 @@ jQuery(document).ready(function($){
     $("#cont_matriz_B").html(generarMatrizHTMLVacia("B", 5, 5));
     
 	setTimeout(function(){
-		$("header").removeClass("height-50")
-		$("footer").removeClass("height-50")
-		$(".ventana_principal").removeClass("opacity0")
-		$("#background").removeClass("blur")
+		$("header").removeClass("height-50");
+		$("footer").removeClass("height-50");
+		$(".ventana_principal").removeClass("opacity0");
+		$("#background").removeClass("blur");
 	},2000)
 });
